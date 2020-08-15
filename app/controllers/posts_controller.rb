805c3b3dd117bfc7a4665249420b979745b67edb
lea jasmine <leajasmine@protonmail.com>
2020-08-15 17:55:41 -0400

    Added create new post + edit post + delete post functionalities

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 
   def index
+    @posts = Post.all.order("created_at DESC")
   end
 
   def new
@@ -17,10 +18,38 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+
+    redirect_to posts_path
+  end
+
+
   private
 
   def post_params
     params.require(:post).permit(:title, :content)
   end
+
 
 end
